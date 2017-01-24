2909d27e11e61ef42b4f5947d7f7c89dbf690b9a
tarlepp <tarmo.leppanen@protacon.com>
2017-01-24 16:30:58 +0200

    Implemented anchor scroll to workshop detailed view.

--- src/modules/workshop/workshop-header.controller.js
+++ src/modules/workshop/workshop-header.controller.js
@@ -7,16 +7,20 @@ export default class WorkshopHeaderController {
    *
    * @param {$state}                    $state
    * @param {$stateParams}              $stateParams
+   * @param {$location}                 $location
+   * @param {$anchorScroll}             $anchorScroll
    * @param {WorkshopSharedDataService} WorkshopSharedDataService
    * @param {WorkshopInterface}         _workshop
    */
   constructor(
-    $state, $stateParams,
+    $state, $stateParams, $location, $anchorScroll,
     WorkshopSharedDataService,
     _workshop,
   ) {
     this.state = $state;
     this.stateParams = $stateParams;
+    this.location = $location;
+    this.anchorScroll = $anchorScroll;
     this.sharedData = WorkshopSharedDataService;
     this.workshop = _workshop;
     this.backgroundImage = require('./tausta.jpg');
@@ -39,6 +43,14 @@ export default class WorkshopHeaderController {
       this.state.go('modules.workshop_list', parameters);
     } else {
       this.state.go('modules.home');
+    }
+  }
+
+  goToAnchor(anchor) {
+    if (this.location.hash() !== anchor) {
+      this.location.hash(anchor);
+    } else {
+      this.anchorScroll();
     }
   }
 }
