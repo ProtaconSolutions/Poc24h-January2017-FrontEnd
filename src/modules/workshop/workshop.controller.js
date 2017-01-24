027c10976e3a5ff31cadbb5228deecf87c990bf6
tarlepp <tarmo.leppanen@protacon.com>
2017-01-24 11:44:35 +0200

    Implemented fake server request on sending contact form.

--- src/modules/workshop/workshop.controller.js
+++ src/modules/workshop/workshop.controller.js
@@ -5,30 +5,57 @@ export default class WorkshopController {
   /**
    * Constructor of the class
    *
+   * @param {$timeout}                  $timeout
    * @param {WorkshopSharedDataService} WorkshopSharedDataService
+   * @param {LoggerService}             LoggerService
    * @param {WorkshopInterface}         _workshop
    * @param {CarBrandInterface[]}       _carBrands
    */
   constructor(
-    WorkshopSharedDataService,
+    $timeout,
+    WorkshopSharedDataService, LoggerService,
     _workshop,
     _carBrands,
   ) {
     const carBrands = [...Array(20).keys()].map(() => _carBrands[Math.floor(Math.random() * _carBrands.length)].name);
 
+    this.timeout = $timeout;
     this.sharedData = WorkshopSharedDataService;
+    this.logger = LoggerService;
     this.workshop = _workshop;
     this.carBrands = [...new Set(carBrands)].sort();
     this.emails = this.workshop.email.split(',').map(email => email.trim());
+    this.loading = false;
 
     this.data = {
       contactType: 'puhelin',
       licensePlateNumber: '',
+      phone: '',
+      email: '',
     }
   }
 
   // Change view action to contact form
   contact() {
     this.sharedData.action = 'contact';
+
+    this.data = {
+      contactType: 'puhelin',
+      licensePlateNumber: '',
+      phone: '',
+      email: '',
+    }
+  }
+
+  // Submit contact form
+  submit() {
+    this.loading = true;
+
+    // Fake some data processing
+    this.timeout(() => {
+      this.logger.success('Yhteydenottopyyntösi on välitetty korjaamolle');
+      this.sharedData.action = 'information';
+      this.loading = false;
+    }, 1500);
   }
 }
