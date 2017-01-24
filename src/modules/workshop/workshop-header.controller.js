6cfa18954b6e0d5081fc11e2860bff6ef742612f
tarlepp <tarmo.leppanen@protacon.com>
2017-01-24 10:22:17 +0200

    Implemented back button functionality from workshop detailed view.

--- src/modules/workshop/workshop-header.controller.js
+++ src/modules/workshop/workshop-header.controller.js
@@ -16,7 +16,20 @@ export default class WorkshopHeaderController {
     this.state = $state;
     this.stateParams = $stateParams;
     this.workshop = _workshop;
-
     this.backgroundImage = require('./tausta.jpg');
+  }
+
+  back() {
+    if (this.stateParams.selected) {
+      const parameters = {
+        serviceType: this.stateParams.selected.serviceType,
+        carBrand: this.stateParams.selected.carBrand,
+        city: this.stateParams.selected.city,
+      };
+
+      this.state.go('modules.workshop_list', parameters);
+    } else {
+      this.state.go('modules.home');
+    }
   }
 }
